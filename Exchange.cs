1caaebd2bd12d7312c7ffd6e40cccde4f50447c3
CyberPunkMetalHead <andreibadoiu@ymail.com>
2023-12-18 13:28:30 +0000

    add all

---
+++ Exchange.cs
@@ -0,0 +1,32 @@
+﻿using ExchangeSharp;
+
+namespace Sharptest
+{
+    public class Exchange
+    {
+        private readonly IExchangeAPI _exchangeAPI;
+        public Exchange()
+        {
+
+            // Initialize the Exchange API within the constructor
+            _exchangeAPI = ExchangeAPI.GetExchangeAPIAsync(ExchangeName.Coinbase).Result;
+            _exchangeAPI.LoadAPIKeys(@"path\to\Keys.bin");
+
+        }
+
+        public async Task<IEnumerable<ExchangeMarket>> GetExchangeInfo()
+        {
+            return await _exchangeAPI.GetMarketSymbolsMetadataAsync();
+        }
+
+        public async Task<ExchangeOrderResult> Buy(ExchangeOrderRequest order)
+        {
+            return await _exchangeAPI.PlaceOrderAsync(order);
+        }
+
+        public async Task<ExchangeDepositDetails> GetDepositDetails(string coin)
+        {
+            return await _exchangeAPI.GetDepositAddressAsync(coin);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using ExchangeSharp;
+using Sharptest;
+
+
+var exchange = new Exchange();
+var marketSymbols = exchange.GetExchangeInfo().Result;
+var depositDetails = await exchange.GetDepositDetails("Ethereum");
+
+Console.WriteLine(depositDetails);
+
+foreach (var symbol in marketSymbols)
+{
+    Console.WriteLine($"Symbol: {symbol.MarketSymbol}, PriceStepSize: {symbol.PriceStepSize}, QuantityStepSize: {symbol.QuantityStepSize}");
+}
+
+ExchangeOrderRequest buyOrder = new()
+{
+    MarketSymbol = "SOLUSDT",
+    Amount = 0.1m,        
+    Price = null,        
+    StopPrice = 0.0m, 
+    IsBuy = true,     
+    IsMargin = false,
+    OrderId = null,
+    ClientOrderId = null,
+    ShouldRoundAmount = true,
+    OrderType = OrderType.Market, 
+    IsPostOnly = null,
+};
+
+Console.WriteLine(await exchange.GetExchangeInfo());
+
+await exchange.Buy(buyOrder);
+
+await Task.Delay(5000);
+
